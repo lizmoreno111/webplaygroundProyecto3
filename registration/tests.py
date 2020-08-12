d61e82c05198243cb3a7bbff58aacf4d9b52fb35
Lizbeth Moreno <lizbethmoreno989@gmail.com>
2020-08-12 16:31:32 -0500

    webplayground

---
+++ registration/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.com', 'test1234')
